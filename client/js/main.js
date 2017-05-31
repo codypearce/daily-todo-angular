7675f9c7af7b3c518aa5770b0715473ba7fbeaaf
codypearce <codyapearce@gmail.com>
2017-05-31 07:39:33 -0600

    crud on todos, repeat todos, main controller

---
+++ client/js/main.js
@@ -0,0 +1,32 @@
+var app = angular.module('dailyTodo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    $http.get('/api/todos')
+      .then(function(data, err) {
+        if(err)
+          console.log(err)
+        $scope.todos = data;
+      })
+
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+          .then(function(data, err) {
+            if(err)
+              console.log(err)
+            $scope.formData = {};
+            $scope.todos = data;
+          })
+    };
+
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+          .then(function(data, err) {
+            if(err)
+              console.log(err)
+            $scope.todos = data;
+          })
+    };
+
+}
