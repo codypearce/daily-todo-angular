dd147e84ee39d58a7f708691f84fab2c3d5afe0d
codypearce <codyapearce@gmail.com>
2017-06-18 06:34:11 -0600

    configure and install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "globals": {
+      "angular": true,
+      "app": true,
+    },
+     "parserOptions": { "ecmaVersion": 6 }
+};
